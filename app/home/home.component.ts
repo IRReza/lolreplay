16f7bb0c83d59c0a8227965695a458e4eb36a43d
lykaiowang <lykaiowang@hotmail.com>
2016-05-11 11:49:25 -0400

    add onGetVideos

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,11 +1,23 @@
 import {Component} from '@angular/core';
+import {YoutubeService} from '../common/youtube.service';
 
 @Component({
   selector: 'home-component',
+  providers:[YoutubeService],
   template: `home`
 })
 export class HomeComponent{
-  constructor(){
 
+  response:string; //String in json format
+
+  constructor(private YoutubeService:YoutubeService){
+
+  }
+
+  onGetVideos(){
+    this.YoutubeService.listVideos().subscribe(
+      res => {console.log(res)},
+      error => {console.log(error)}
+    )
   }
 }
