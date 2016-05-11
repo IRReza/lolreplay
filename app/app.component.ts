fd83c63ca417527a06ee1b7dee02559417ad9a77
lykaiowang <lykaiowang@hotmail.com>
2016-05-10 21:06:18 -0400

    setup router

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,23 @@
 import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES, Routes, Router} from '@angular/router';
+import {NavComponent} from './common/nav.component';
+import {HomeComponent} from './home/home.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    directives: [ROUTER_DIRECTIVES, NavComponent],
+    template: `
+      <nav-component></nav-component>
+      <div class="container-fluid">
+        <router-outlet></router-outlet>
+      </div>
+    `
 })
-export class AppComponent { }
+@Routes([
+  {path: '/', component: HomeComponent}
+])
+export class AppComponent {
+  constructor(private router:Router){
+  }
+
+}
---
+++ app/home/home.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'home-component',
+  template: `home`
+})
+export class HomeComponent{
+  constructor(){
+
+  }
+}
