90a0ab2b434470e7f1709e91a906aaa956d760f4
lykaiowang <lykaiowang@hotmail.com>
2016-05-11 12:43:28 -0400

    inject service into home page

--- app/common/youtube.service.ts
+++ app/common/youtube.service.ts
@@ -2,7 +2,8 @@ import {Injectable} from '@angular/core';
 import API_KEYS from '../config/api';
 import YOUTUBE from '../config/youtube';
 import {Observable} from 'rxjs';
-import{Http} from '@angular/http';
+import{Http, Response} from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class YoutubeService{
@@ -13,15 +14,24 @@ export class YoutubeService{
 
   }
 
-  listVideos(): Observable<any>{
+  listVideos(pageToken?:string): Observable<any>{
     let url = this.baseURL + `search?` +
     `part=snippet&` +
     `maxResults=30&` +
-    `order=viewCount&` +
+    `order=date&` +
     `channelId=${YOUTUBE.channelId}&` +
     `type=video&` +
     `key=${API_KEYS.google}&`
 
-    return this.http.get(url).map(res => res.json());
+    if (pageToken) url += `pageToken=${pageToken}&`
+    return this.http.get(url).map(this.extractData);
+  }
+
+  private extractData(res: Response) {
+    if (res.status < 200 || res.status >= 300) {
+      throw new Error('Bad response status: ' + res.status);
+    }
+    let body = res.json();
+    return body || { };
   }
 }
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,23 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {YoutubeService} from '../common/youtube.service';
 
 @Component({
   selector: 'home-component',
   providers:[YoutubeService],
-  template: `home`
+  template: `
+    <div *ngFor="let video of response.items">
+      <p>{{video.snippet.title}}</p>
+    </div>
+  `
 })
-export class HomeComponent{
+export class HomeComponent implements OnInit{
 
-  response:string; //String in json format
+  response:string = ""; //String in json format
+  currentPage:number = 1;
 
   constructor(private YoutubeService:YoutubeService){
-
   }
 
-  onGetVideos(){
-    this.YoutubeService.listVideos().subscribe(
-      res => {console.log(res)},
+  ngOnInit() {
+    this.getVideos();
+  }
+
+  getVideos(pageToken?:string){
+    this.YoutubeService.listVideos(pageToken).subscribe(
+      res => {this.response = res},
       error => {console.log(error)}
     )
   }
+
+  goToPage(targetPage:number){
+
+  }
+
 }
