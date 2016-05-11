057e762d21af20cad36041d903ebdfa302bf6e2b
lykaiowang <lykaiowang@hotmail.com>
2016-05-11 14:42:07 -0400

    add caching to riot service

--- app/common/riot.service.ts
+++ app/common/riot.service.ts
@@ -2,11 +2,13 @@ import {Injectable} from '@angular/core'
 import API_KEYS from '../config/api'
 import {Champion} from './champion.interface'
 import {Http, Response} from '@angular/http'
-import {Observable} from 'rxjs'
+import {Observable} from 'rxjs/Observable'
 import 'rxjs/Rx'
 
 @Injectable()
 export class RiotService{
+
+  private static championList:Champion[] = [];
 
   constructor(private http:Http){
   }
@@ -20,19 +22,23 @@ export class RiotService{
   }
 
   getChampionList():Observable<any>{
+    //Caching
+    if (RiotService.championList.length > 0)
+      return Observable.of(RiotService.championList)
+
     let url = this.getBaseUrl('global') + 'static-data/na/v1.2/champion?' + `&api_key=${API_KEYS.riot}`
 
     function extractData(res: Response) {
+      console.log("extracting..")
       if (res.status < 200 || res.status >= 300) {
         throw new Error('Bad response status: ' + res.status)
       }
       let data = res.json().data
-      let list:Champion[] = []
-      
+
       Object.keys(data).map(function(key, index) {
-        list.push(data[key])
+        RiotService.championList.push(data[key])
       })
-      return list;
+      return RiotService.championList;
     }
 
     return this.http.get(url).map(extractData)
